35efcee2171af1be73cbfeadba3ed26b7d2b3a14
Nikola1993 <31863147+Nikola1993@users.noreply.github.com>
2018-05-09 18:07:12 +0200

    Log in Design

---
+++ ToDo/Controllers/LogInVC.swift
@@ -0,0 +1,32 @@
+//
+//  LogInVC.swift
+//  ToDo
+//
+//  Created by Nikola Popovic on 5/9/18.
+//  Copyright © 2018 Nikola Popovic. All rights reserved.
+//
+
+import UIKit
+
+class LogInVC: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var okButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       setButton()
+    }
+
+    @IBAction func okAction(_ sender: UIButton) {
+        
+    }
+    
+    func setButton(){
+        okButton.layer.cornerRadius = okButton.frame.size.height / 2
+        okButton.layer.borderWidth = 2.0
+    }
+}
